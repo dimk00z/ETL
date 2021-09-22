ef90d4ef47cc9ffc32b98cdb23cc9f38f9e52578
Dimk00z <dimk00z@gmail.com>
2021-09-22 15:39:12 +0300

    rename loaders

--- postgres_to_es/connections.py
+++ postgres_to_es/connections.py
@@ -18,13 +18,13 @@ def backoff_hdlr(details):
 
 
 @backoff.on_exception(backoff.expo, (psycopg2.Error, psycopg2.OperationalError), on_backoff=backoff_hdlr)
-def load_postges(postgres_settings: dict) -> psycopg2.extensions.connection:
+def connect_to_postges(postgres_settings: dict) -> psycopg2.extensions.connection:
     pg_conn: psycopg2.extensions.connection = psycopg2.connect(**postgres_settings, cursor_factory=DictCursor)
     return pg_conn
 
 
 @backoff.on_exception(backoff.expo, (ValueError, NewConnectionError), on_backoff=backoff_hdlr)
-def load_elastic(host: str) -> elasticsearch.client.Elasticsearch:
+def connect_to_elastic(host: str) -> elasticsearch.client.Elasticsearch:
     es: elasticsearch.client.Elasticsearch = elasticsearch.Elasticsearch(host, verify_certs=True)
     if not es.ping():
         raise ValueError("Connection failed")
@@ -32,7 +32,7 @@ def load_elastic(host: str) -> elasticsearch.client.Elasticsearch:
 
 
 @backoff.on_exception(backoff.expo, (RedisConnectionError), on_backoff=backoff_hdlr)
-def load_redis(settings: dict) -> Redis:
+def connect_to_redis(settings: dict) -> Redis:
     redis_adapter: Redis = Redis(**settings)
     redis_adapter.ping()
     return redis_adapter
--- postgres_to_es/main.py
+++ postgres_to_es/main.py
@@ -1,22 +1,49 @@
+import atexit
 import logging
+from datetime import datetime
 from typing import Dict, List, Tuple
 
 import elasticsearch
 import psycopg2
+from redis import Redis
 
-from loaders import load_elastic, load_postges
+from connections import (
+    close_pg_conn,
+    connect_to_elastic,
+    connect_to_postges,
+    connect_to_redis,
+)
 from setting_loaders import load_etl_settings
 
 logger = logging.getLogger(__file__)
 logging.basicConfig(level=logging.INFO)
 
 
-def main():
-    postgres_settings, elastic_settings = load_etl_settings()
-    pg_conn: psycopg2.extensions.connection = load_postges(postgres_settings.dict())
-    es: elasticsearch.client.Elasticsearch = load_elastic(elastic_settings.host)
+def load():
+    pass
 
-    pg_conn.close()
+
+def start_etl(pg_conn, es):
+    pass
+    loader = load()
+
+    # transformer = transform(loader, Transformer())
+
+    # extractor = extract(transformer, PostgresExtractor(pg_conn))
+
+    # etl(extractor)
+
+
+def main():
+    postgres_settings, elastic_settings, redis_settings = load_etl_settings()
+    pg_conn: psycopg2.extensions.connection = connect_to_postges(postgres_settings.dict())
+    es: elasticsearch.client.Elasticsearch = connect_to_elastic(elastic_settings.host)
+    redis_adapter: Redis = connect_to_redis(redis_settings.dict())
+    atexit.register(close_pg_conn, pg_conn=pg_conn)
+    while True:
+        logging.info(f"Start etl_app at {datetime.now()}")
+        start_etl(pg_conn=pg_conn, es=es)
+        break
 
 
 if __name__ == "__main__":

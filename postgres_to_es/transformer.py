01d09e288f3b9c07fc1f3e5425562b0007b5eddb
Dimk00z <dimk00z@gmail.com>
2021-09-24 15:14:21 +0300

    add transformer class

--- postgres_to_es/transformer.py
+++ postgres_to_es/transformer.py
@@ -1,3 +1,31 @@
+from typing import List
+
+
 class Transformer:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, extracted_movies: dict) -> None:
+        self.extracted_movies = extracted_movies
+
+    def transform_movies(self) -> List[dict]:
+        transformed_movies: List[dict] = []
+        for film_id, film_work in self.extracted_movies.items():
+            # print(film_id, film_work)
+            transformed_movie = {
+                "id": film_id,
+                "imdb_rating": film_work.rating,
+                "title": film_work.title,
+                "description": film_work.description,
+                "genre": [genre for genre in film_work.genres],
+                "director": [director.full_name for director in film_work.directors],
+            }
+            if film_work.writers:
+                transformed_movie["writers"] = [
+                    {"id": writer.id, "name": writer.full_name} for writer in film_work.writers
+                ]
+            if film_work.actors:
+                transformed_movie["actors"] = [
+                    {"id": actor.id, "name": actor.full_name} for actor in film_work.actors
+                ]
+            transformed_movies.append(transformed_movie)
+
+        # print(transformed_movies)
+        return transformed_movies

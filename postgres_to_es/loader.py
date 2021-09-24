08ca873b8c22d1958cd30ce5589a4d77bd27bb9b
Dimk00z <dimk00z@gmail.com>
2021-09-24 15:57:53 +0300

    fix scheme

--- postgres_to_es/loader.py
+++ postgres_to_es/loader.py
@@ -47,12 +47,20 @@ class ESLoader:
                 "properties": {
                     "id": {"type": "keyword"},
                     "imdb_rating": {"type": "float"},
-                    "genre": {"type": "keyword"},
+                    "genres": {"type": "keyword"},
                     "title": {"type": "text", "analyzer": "ru_en", "fields": {"raw": {"type": "keyword"}}},
                     "description": {"type": "text", "analyzer": "ru_en"},
-                    "director": {"type": "text", "analyzer": "ru_en"},
+                    # "director": {"type": "text", "analyzer": "ru_en"},
                     "actors_names": {"type": "text", "analyzer": "ru_en"},
                     "writers_names": {"type": "text", "analyzer": "ru_en"},
+                    "directors": {
+                        "type": "nested",
+                        "dynamic": "strict",
+                        "properties": {
+                            "id": {"type": "keyword"},
+                            "name": {"type": "text", "analyzer": "ru_en"},
+                        },
+                    },
                     "actors": {
                         "type": "nested",
                         "dynamic": "strict",
--- postgres_to_es/transformer.py
+++ postgres_to_es/transformer.py
@@ -14,17 +14,22 @@ class Transformer:
                 "imdb_rating": film_work.rating,
                 "title": film_work.title,
                 "description": film_work.description,
-                "genre": [genre for genre in film_work.genres],
-                "director": [director.full_name for director in film_work.directors],
+                "genres": [genre for genre in film_work.genres],
+                # "director": [director.full_name for director in film_work.directors],
             }
-            if film_work.writers:
-                transformed_movie["writers"] = [
-                    {"id": writer.id, "name": writer.full_name} for writer in film_work.writers
-                ]
-            if film_work.actors:
-                transformed_movie["actors"] = [
-                    {"id": actor.id, "name": actor.full_name} for actor in film_work.actors
-                ]
+            for persons in ("writers", "actors", "directors"):
+                if getattr(film_work, persons):
+                    transformed_movie[persons] = [
+                        {"id": person.id, "name": person.full_name} for person in getattr(film_work, persons)
+                    ]
+            # if film_work.writers:
+            #     transformed_movie["writers"] = [
+            #         {"id": writer.id, "name": writer.full_name} for writer in film_work.writers
+            #     ]
+            # if film_work.actors:
+            #     transformed_movie["actors"] = [
+            #         {"id": actor.id, "name": actor.full_name} for actor in film_work.actors
+            #     ]
             transformed_movies.append(transformed_movie)
 
         # print(transformed_movies)

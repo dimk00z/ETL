e90b1b47f2d502ed9f89bffc8c01a589e08c487e
Dimk00z <dimk00z@gmail.com>
2021-09-25 20:49:32 +0300

    fix with new model

--- postgres_to_es/transformer.py
+++ postgres_to_es/transformer.py
@@ -7,30 +7,20 @@ class Transformer:
 
     def transform_movies(self) -> List[dict]:
         transformed_movies: List[dict] = []
-        for film_id, film_work in self.extracted_movies.items():
-            # print(film_id, film_work)
+        for film_work in self.extracted_movies:
             transformed_movie = {
-                "id": film_id,
+                "id": film_work.id,
                 "imdb_rating": film_work.rating,
                 "title": film_work.title,
                 "description": film_work.description,
                 "genres": [genre for genre in film_work.genres],
-                # "director": [director.full_name for director in film_work.directors],
             }
             for persons in ("writers", "actors", "directors"):
                 if getattr(film_work, persons):
                     transformed_movie[persons] = [
                         {"id": person.id, "name": person.full_name} for person in getattr(film_work, persons)
                     ]
-            # if film_work.writers:
-            #     transformed_movie["writers"] = [
-            #         {"id": writer.id, "name": writer.full_name} for writer in film_work.writers
-            #     ]
-            # if film_work.actors:
-            #     transformed_movie["actors"] = [
-            #         {"id": actor.id, "name": actor.full_name} for actor in film_work.actors
-            #     ]
+
             transformed_movies.append(transformed_movie)
 
-        # print(transformed_movies)
         return transformed_movies

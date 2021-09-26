a75f8983fb787cebe2fea03ef40474efcc0dad4b
Dimk00z <dimk00z@gmail.com>
2021-09-26 17:48:22 +0300

    add signals

--- movies_admin/movies/signals.py
+++ movies_admin/movies/signals.py
@@ -1,10 +1,30 @@
 import datetime
 
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
+from django.utils.timezone import now
 
 
-@receiver(post_save, sender="movies.Person")
-def congratulatory(sender, instance, created, **kwargs):
-    if created and instance.birth_date == datetime.date.today():
-        print(f"У {instance.full_name} сегодня день рождения! 🥳")
+def update_movie(instance):
+    instance.film_work.updated_at = now
+    instance.film_work.save()
+
+
+@receiver(post_save, sender="movies.GenreFilmWork")
+def save_genre_film(sender, instance, **kwargs):
+    update_movie(instance)
+
+
+@receiver(post_save, sender="movies.PersonFilmWork")
+def save_person_film(sender, instance, **kwargs):
+    update_movie(instance)
+
+
+@receiver(pre_delete, sender="movies.GenreFilmWork")
+def delete_genre_film(sender, instance, **kwargs):
+    update_movie(instance)
+
+
+@receiver(pre_delete, sender="movies.PersonFilmWork")
+def delete_person_film(sender, instance, **kwargs):
+    update_movie(instance)

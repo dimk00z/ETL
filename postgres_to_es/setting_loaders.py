042303b8505acd2eb2aa7dde893b26c9304e4aed
Dimk00z <dimk00z@gmail.com>
2021-09-27 09:53:11 +0300

    fix env options

--- postgres_to_es/setting_loaders.py
+++ postgres_to_es/setting_loaders.py
@@ -9,8 +9,8 @@ class PostgresSettings(BaseSettings):
     dbname: str = Field(..., env="POSTGRES_DB")
     user: str = Field(..., env="POSTGRES_USER")
     password: str = Field(..., env="POSTGRES_PASSWORD")
-    host: str = Field(..., env="DB_HOST")
-    port: str = Field(..., env="DB_PORT")
+    host: str = Field(..., env="POSTGRES_HOST")
+    port: str = Field(..., env="POSTGRES_PORT")
     options: str = "-c search_path=content"
 
     class Config:

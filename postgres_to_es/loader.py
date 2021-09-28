69d2ff893da4b43d5ed4b23617beef436739f731
Dimk00z <dimk00z@gmail.com>
2021-09-28 11:20:58 +0300

    add backoff to bulk_index

--- postgres_to_es/loader.py
+++ postgres_to_es/loader.py
@@ -1,8 +1,11 @@
 import logging
 from typing import List
 
+import backoff
 from elasticsearch import helpers
 from elasticsearch.client import Elasticsearch as ES_client
+
+from connections import backoff_hdlr
 
 
 class ESLoader:
@@ -95,10 +98,10 @@ class ESLoader:
             self.created_index = index_exist
             return self.created_index
 
+    @backoff.on_exception(backoff.expo, (elasticsearch.ElasticsearchException), on_backoff=backoff_hdlr)
     def bulk_index(self, transformed_data: List[dict], last_state: str) -> None:
         try:
             if last_state:
-
                 remove_actions = [
                     {
                         "_id": transormed_film["_id"],
@@ -116,5 +119,5 @@ class ESLoader:
             helpers.bulk(
                 self.es, actions=transformed_data, index=self.index_name, refresh=True, raise_on_error=True
             )
-        except elasticsearch.ElasticsearchException as e:
-            logging.error(e)
+        except elasticsearch.ElasticsearchException as elasticsearch_exception:
+            logging.error(elasticsearch_exception)
